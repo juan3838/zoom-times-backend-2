99e05563663308e94f61a86ae4a6bfe3b115f588
juan3838 <juan.larrocca38@gmail.com>
2025-09-03 16:07:14 -0300

    Update get_noticias.php

--- get_noticias.php
+++ get_noticias.php
@@ -1,7 +1,7 @@
 <?php
 require 'db.php';
 
-$stmt = $pdo->query("SELECT id_noticia, titulo, subtitulo, fecha_publicacion FROM noticias WHERE estado='publicada' ORDER BY fecha_publicacion DESC");
+$stmt = $pdo->query("SELECT id_noticia, titulo, bajada, contenido, fecha_publicacion FROM noticias WHERE estado='publicada' ORDER BY fecha_publicacion DESC");
 $noticias = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 header('Content-Type: application/json');
